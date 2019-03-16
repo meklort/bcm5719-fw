11bfe8113439e2a6724422db899724bbee8341ae
Evan Lojewski <github@meklort.com>
2019-03-15 19:36:34 -0600

    Begin adding stage2 init code.

--- stage2/main.c
+++ stage2/main.c
@@ -52,26 +52,35 @@ void init_mii(void)
     // Set MII_REG_CONTROL to RESET; wait until RESET bit clears.
     MII_reset(phy);
 
-    // Mask REG_MDI_CONTROL bits 0x4. Meaning unknown.
-    // uint16_t mdi_control = MII_readRegister(phy, (mii_reg_t)REG_MDI_CONTROL);
+    // Mask REG_MDI_CONTROL bits 0x4. MDI Select must be cleared to enable Auto-Access mode for the MAC.
+    DEVICE.MdiControl.r32 &= ~0x4;
 
     // Set MII (SHADOW 0x05):0x1C as desired. e.g. CLK125_OUTPUT_ENABLE, SIGDET_DEASSERT_TIMER_LENGTHEN, DISABLE_LOW_POWER_10BASET_LINK_MODE, LOW_POWER_ENC_DISABLE.
     // uint16_t shadow = MII_readRegister(phy, )
+    RegMIISpareControl3_t sc3;
+    sc3.r16 = 0; // MII_readRegister(phy, (mii_reg_t)REG_MII_SPARE_CONTROL_3);
+    sc3.bits.CLK125OutputEnable = 1;
+    sc3.bits.SIGDETDeassertTimerLengthen = 1;
+    sc3.bits.DisableLowPowerTOBase_TLinkMode = 1;
+    sc3.bits.LowPowerENCDisable = 1;
+    MII_writeRegister(phy, (mii_reg_t)REG_MII_SPARE_CONTROL_3, sc3.r16);
 
     // Set MII_REG_1000BASE_T_CONTROL as desired (e.g. ADVERTISE_FULL_DUPLEX|ADVERTISE_HALF_DUPLEX).
+    RegMII1000baseTControl_t gig_control;
+    gig_control.r16 = 0;
+    gig_control.bits.Advertise1000BASE_THalfDuplex = 1;
+    gig_control.bits.Advertise1000BASE_TFullDuplex = 1;
+    MII_writeRegister(phy, (mii_reg_t)REG_MII_1000BASE_T_CONTROL, gig_control.r16);
 
     // Set MII_REG_AUTO_NEGOTIATION_ADVERTISEMENT as desired (pretty much enable PROTOCOL_SELECT__IEEE_802_3 and all the full/half duplex options).
     RegMIIAutonegotiationAdvertisement_t auto_neg_advert;
-    auto_neg_advert.r16 = MII_readRegister(phy, (mii_reg_t)REG_MII_AUTONEGOTIATION_ADVERTISEMENT);
+    auto_neg_advert.r16 = 0; //MII_readRegister(phy, (mii_reg_t)REG_MII_AUTONEGOTIATION_ADVERTISEMENT);
     auto_neg_advert.bits.ProtocolSelect = MII_AUTONEGOTIATION_ADVERTISEMENT_PROTOCOL_SELECT_IEEE_802_3;
     auto_neg_advert.bits._10BASE_THalfDuplexCapable = 1;
     auto_neg_advert.bits._10BASE_TFullDuplexCapable = 1;
     auto_neg_advert.bits._100BASE_TXHalfDuplexCapable = 1;
     auto_neg_advert.bits._100BASE_TXFullDuplexCapable = 1;
     MII_writeRegister(phy, (mii_reg_t)REG_MII_AUTONEGOTIATION_ADVERTISEMENT, auto_neg_advert.r16);
-
-
-    // Set MII_REG_CONTROL to AUTO_NEGOTIATION_RESTART|AUTO_NEGOTIATION_ENABLE
 }
 
 int main()
@@ -79,6 +88,16 @@ int main()
     init_mii();
 
     // Set REG_EMAC_MODE__PORT_MODE based on REG_STATUS__ETHERNET_LINK_STATUS (GPHY) or REG_SGMII_STATUS__LINK_STATUS (SERDES).
+    if(DEVICE.SgmiiStatus.bits.MediaSelectionMode)
+    {
+        // SERDES
+        DEVICE.EmacMode.bits.PortMode = DEVICE.SgmiiStatus.bits.LinkStatus;
+    }
+    else
+    {
+        // GPHY
+        DEVICE.EmacMode.bits.PortMode = DEVICE.Status.bits.EthernetLinkStatus;
+    }
 
     // Set REG_LINK_AWARE_POWER_MODE_CLOCK_POLICY__MAC_CLOCK_SWITCH__6_25MHZ.
     DEVICE.LinkAwarePowerModeClockPolicy.bits.MACClockSwitch = DEVICE_LINK_AWARE_POWER_MODE_CLOCK_POLICY_MAC_CLOCK_SWITCH_6_25MHZ;
