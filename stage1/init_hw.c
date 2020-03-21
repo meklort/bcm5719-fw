45cf153fd76f6c501982de0ee810637e8761ffc2
Evan Lojewski <github@meklort.com>
2020-03-21 11:39:45 -0600

    stage1: Refactor init_hw to remove direct access to DEVICE.

--- stage1/init_hw.c
+++ stage1/init_hw.c
@@ -78,7 +78,7 @@ void *memset(void *s, int c, size_t n)
 
 void init_mii_function0(volatile DEVICE_t *device)
 {
-    if (0 == DEVICE.Status.bits.FunctionNumber)
+    if (0 == device->Status.bits.FunctionNumber)
     {
         reportStatus(STATUS_INIT_HW, 0xf1);
 
@@ -167,43 +167,43 @@ void early_init_hw(void)
     memset((void *)&SDBCACHE, 0, REG_SDBCACHE_SIZE);
 }
 
-void init_mac(NVRAMContents_t *nvram)
+void init_mac(volatile DEVICE_t *device, NVRAMContents_t *nvram)
 {
-    int function = DEVICE.Status.bits.FunctionNumber;
+    int function = device->Status.bits.FunctionNumber;
     uint32_t *mac0 = nvram->info.macAddr0;
     uint32_t *my_mac = mac0; // default.
-    DEVICE.EmacMacAddresses0High.r32 = mac0[0];
-    DEVICE.EmacMacAddresses0Low.r32 = mac0[1];
+    device->EmacMacAddresses0High.r32 = mac0[0];
+    device->EmacMacAddresses0Low.r32  = mac0[1];
 
     uint32_t *mac1 = nvram->info.macAddr1;
     if (1 == function)
     {
         my_mac = mac1;
     }
-    DEVICE.EmacMacAddresses1High.r32 = mac1[0];
-    DEVICE.EmacMacAddresses1Low.r32 = mac1[1];
+    device->EmacMacAddresses1High.r32 = mac1[0];
+    device->EmacMacAddresses1Low.r32  = mac1[1];
 
     uint32_t *mac2 = nvram->info2.macAddr2;
     if (2 == function)
     {
         my_mac = mac2;
     }
-    DEVICE.EmacMacAddresses2High.r32 = mac2[0];
-    DEVICE.EmacMacAddresses2Low.r32 = mac2[1];
+    device->EmacMacAddresses2High.r32 = mac2[0];
+    device->EmacMacAddresses2Low.r32  = mac2[1];
 
     uint32_t *mac3 = nvram->info2.macAddr3;
     if (3 == function)
     {
         my_mac = mac3;
     }
-    DEVICE.EmacMacAddresses3High.r32 = mac3[0];
-    DEVICE.EmacMacAddresses3Low.r32 = mac3[1];
+    device->EmacMacAddresses3High.r32 = mac3[0];
+    device->EmacMacAddresses3Low.r32  = mac3[1];
 
     // Store mac / serial number.
-    DEVICE.PciSerialNumberHigh.r32 = my_mac[0];
+    device->PciSerialNumberHigh.r32 = my_mac[0];
     GEN.GenMacAddrHighMbox.r32 = my_mac[0];
 
-    DEVICE.PciSerialNumberLow.r32 = my_mac[1];
+    device->PciSerialNumberLow.r32  = my_mac[1];
     GEN.GenMacAddrLowMbox.r32 = my_mac[1];
 }
 
@@ -223,11 +223,11 @@ uint32_t translate_power_budget(uint16_t raw)
     return translator.r32;
 }
 
-void init_power(NVRAMContents_t *nvram)
+void init_power(volatile DEVICE_t *device, NVRAMContents_t *nvram)
 {
     // PCI power dissipated / consumed
-    DEVICE.PciPowerConsumptionInfo.r32 = nvram->info.powerConsumed;
-    DEVICE.PciPowerDissipatedInfo.r32 = nvram->info.powerDissipated;
+    device->PciPowerConsumptionInfo.r32 = nvram->info.powerConsumed;
+    device->PciPowerDissipatedInfo.r32  = nvram->info.powerDissipated;
 
     // Power Budget
     uint32_t pb_raw0 = (nvram->info.powerBudget0) & 0xffff;
@@ -239,14 +239,14 @@ void init_power(NVRAMContents_t *nvram)
     uint32_t pb_raw6 = (nvram->info.powerBudget3) & 0xffff;
     uint32_t pb_raw7 = (nvram->info.powerBudget3) >> 16;
 
-    DEVICE.PciPowerBudget0.r32 = translate_power_budget(pb_raw0);
-    DEVICE.PciPowerBudget1.r32 = translate_power_budget(pb_raw1);
-    DEVICE.PciPowerBudget2.r32 = translate_power_budget(pb_raw2);
-    DEVICE.PciPowerBudget3.r32 = translate_power_budget(pb_raw3);
-    DEVICE.PciPowerBudget4.r32 = translate_power_budget(pb_raw4);
-    DEVICE.PciPowerBudget5.r32 = translate_power_budget(pb_raw5);
-    DEVICE.PciPowerBudget6.r32 = translate_power_budget(pb_raw6);
-    DEVICE.PciPowerBudget7.r32 = translate_power_budget(pb_raw7);
+    device->PciPowerBudget0.r32 = translate_power_budget(pb_raw0);
+    device->PciPowerBudget1.r32 = translate_power_budget(pb_raw1);
+    device->PciPowerBudget2.r32 = translate_power_budget(pb_raw2);
+    device->PciPowerBudget3.r32 = translate_power_budget(pb_raw3);
+    device->PciPowerBudget4.r32 = translate_power_budget(pb_raw4);
+    device->PciPowerBudget5.r32 = translate_power_budget(pb_raw5);
+    device->PciPowerBudget6.r32 = translate_power_budget(pb_raw6);
+    device->PciPowerBudget7.r32 = translate_power_budget(pb_raw7);
 }
 
 uint16_t nvm_get_subsystem_device(volatile DEVICE_t *device, NVRAMContents_t *nvram)
@@ -286,21 +286,21 @@ void init_pci(volatile DEVICE_t *device, NVRAMContents_t *nvram)
     vendor_device.r32 = 0;
     vendor_device.bits.DeviceID = nvram->info.deviceID;
     vendor_device.bits.VendorID = nvram->info.vendorID;
-    DEVICE.PciVendorDeviceId = vendor_device;
+    device->PciVendorDeviceId = vendor_device;
 
     // PCI Subsystem
     RegDEVICEPciSubsystemId_t subsystem;
     subsystem.r32 = 0;
     subsystem.bits.SubsystemVendorID = nvram->info.subsystemVendorID;
     subsystem.bits.SubsystemID = nvm_get_subsystem_device(device, nvram);
-    DEVICE.PciSubsystemId = subsystem;
+    device->PciSubsystemId = subsystem;
 
     // RegDEVICEPciClassCodeRevision_t partially from REG_CHIP_ID
 }
 
-void init_gen(NVRAMContents_t *nvram)
+void init_gen(volatile DEVICE_t *device, NVRAMContents_t* nvram)
 {
-    int function = DEVICE.Status.bits.FunctionNumber;
+    int function = device->Status.bits.FunctionNumber;
     uint32_t cfg_feature;
     uint32_t cfg_hw;
     uint32_t cfg_hw2;
@@ -345,18 +345,18 @@ void load_nvm_config(volatile DEVICE_t *device, NVRAMContents_t *nvram)
     // Load information from NVM, set various registers + mem
 
     // MAC Addr, serial number
-    init_mac(nvram);
+    init_mac(device, nvram);
 
     // firmware revision
     // mfrDate
 
     // Power
-    init_power(nvram);
+    init_power(device, nvram);
 
     // REG_PCI_SUBSYSTEM_ID, vendor, class, rev
     init_pci(device, nvram);
 
-    init_gen(nvram);
+    init_gen(device, nvram);
 }
 
 void init_hw(volatile DEVICE_t *device, NVRAMContents_t *nvram)
@@ -365,51 +365,50 @@ void init_hw(volatile DEVICE_t *device, NVRAMContents_t *nvram)
     // Misc regs init
 
     // Mask REG 0x64C0 bits 0x7FF, or bits 0x0010. This register is unknown.
-    DEVICE._64c0.r32 = (DEVICE._64c0.r32 & ~0x7FFu) | 0x10u;
+    device->_64c0.r32 = (device->_64c0.r32 & ~0x7FFu) | 0x10u;
 
     // Set unknown REG 0x64C8 to 0x1004.
-    DEVICE._64c8.r32 = 0x00001004u;
+    device->_64c8.r32 = 0x00001004u;
 
     // Enable MAC clock speed override
     RegDEVICEClockSpeedOverridePolicy_t clockspeed;
     clockspeed.r32 = 0u;
     clockspeed.bits.MACClockSpeedOverrideEnabled = 1u;
-    DEVICE.ClockSpeedOverridePolicy = clockspeed;
+    device->ClockSpeedOverridePolicy = clockspeed;
 
     // Mask REG 0x64DC bits 0x0F, or bits 0x01. Unknown.
-    DEVICE._64dc.r32 = (DEVICE._64dc.r32 & ~0xFu) | 0x01u;
+    device->_64dc.r32 = (device->_64dc.r32 & ~0xFu) | 0x01u;
 
     // Mask REG 0x64DC bits 0xC00, set ... TODO
     // value from talos: 0x00315E42
-    DEVICE._64dc.r32 &= ~0xC00;
+    device->_64dc.r32 &= ~0xC00;
 
     // Unknown stuff involving REG 0x6530, REG 0x65F4, depends on config, TODO
     // Value from Talos:0x6530z 0x6530 -> 0x00000000, 0x65F4 -> 0x00000109.
 
     // REG_LSO_NONLSO_BD_READ_DMA_CORRUPTION_ENABLE_CONTROL: Set BD and NonLSO
     // fields to 4K.
-    RegDEVICELsoNonlsoBdReadDmaCorruptionEnableControl_t reglso = DEVICE.LsoNonlsoBdReadDmaCorruptionEnableControl;
+    RegDEVICELsoNonlsoBdReadDmaCorruptionEnableControl_t reglso = device->LsoNonlsoBdReadDmaCorruptionEnableControl;
     reglso.bits.PCIRequestBurstLengthforBDRDMAEngine = DEVICE_LSO_NONLSO_BD_READ_DMA_CORRUPTION_ENABLE_CONTROL_PCI_REQUEST_BURST_LENGTH_FOR_BD_RDMA_ENGINE_4K;
-    reglso.bits.PCIRequestBurstLengthforNonLSORDMAEngine =
-        DEVICE_LSO_NONLSO_BD_READ_DMA_CORRUPTION_ENABLE_CONTROL_PCI_REQUEST_BURST_LENGTH_FOR_NONLSO_RDMA_ENGINE_4K;
-    DEVICE.LsoNonlsoBdReadDmaCorruptionEnableControl = reglso;
+    reglso.bits.PCIRequestBurstLengthforNonLSORDMAEngine = DEVICE_LSO_NONLSO_BD_READ_DMA_CORRUPTION_ENABLE_CONTROL_PCI_REQUEST_BURST_LENGTH_FOR_NONLSO_RDMA_ENGINE_4K;
+    device->LsoNonlsoBdReadDmaCorruptionEnableControl = reglso;
 
     // Disable ECC.
-    RegDEVICEGphyStrap_t gphyStrap = DEVICE.GphyStrap;
+    RegDEVICEGphyStrap_t gphyStrap = device->GphyStrap;
     gphyStrap.bits.TXMBUFECCEnable = 0;
     gphyStrap.bits.RXMBUFECCEnable = 0;
     gphyStrap.bits.RXCPUSPADECCEnable = 0;
-    DEVICE.GphyStrap = gphyStrap;
+    device->GphyStrap = gphyStrap;
 
     // LED Control
     // Value from Talos: 0x00000880: DEVICE_LED_CONTROL_LED_STATUS_1000_MASK | DEVICE_LED_CONTROL_LED_MODE_PHY_MODE_1
-    RegDEVICELedControl_t ledControl = DEVICE.LedControl;
+    RegDEVICELedControl_t ledControl = device->LedControl;
     ledControl.bits.LEDMode = DEVICE_LED_CONTROL_LED_MODE_PHY_MODE_1;
-    DEVICE.LedControl = ledControl;
+    device->LedControl = ledControl;
 
     // MISC Local Control
     // Value from Talos: 0x00020001, reserved bits
-    DEVICE.MiscellaneousLocalControl.bits.AutoSEEPROMAccess = 0; // Bit is not set on talos II
+    device->MiscellaneousLocalControl.bits.AutoSEEPROMAccess = 0; // Bit is not set on talos II
 
     // Set REG_EAV_REF_CLOCK_CONTROL as desired. This is initialized from
     // CFG_HW; the TIMESYNC_GPIO_MAPPING, APE_GPIO_{0,1,2,3} fields within it
@@ -422,26 +421,26 @@ void init_hw(volatile DEVICE_t *device, NVRAMContents_t *nvram)
     ref_clock.bits.APEGPIO1Mapping = cfg_hw.bits.APEGPIO1Mapping;
     ref_clock.bits.APEGPIO2Mapping = cfg_hw.bits.APEGPIO2Mapping;
     ref_clock.bits.APEGPIO3Mapping = cfg_hw.bits.APEGPIO3Mapping;
-    DEVICE.EavRefClockControl = ref_clock;
+    device->EavRefClockControl = ref_clock;
 
     // Optionally enable REG_GRC_MODE_CONTROL__TIME_SYNC_MODE_ENABLE.
     // Value from Talos: 0x00130034
     // Bit is not set on Talos w/ default firmware, disabled for now.
-    DEVICE.GrcModeControl.bits.TimeSyncModeEnable = 0;
+    device->GrcModeControl.bits.TimeSyncModeEnable = 0;
 
     // Enable const clock for MII
-    DEVICE.MiiMode.bits.ConstantMDIO_DIV_MDCClockSpeed = 1;
+    device->MiiMode.bits.ConstantMDIO_DIV_MDCClockSpeed = 1;
 
     // Set or clear REG_GPHY_CONTROL_STATUS__SWITCHING_REGULATOR_POWER_DOWN as
     // desired.
     // Value from Talos:  0x02C01000
-    DEVICE.GphyControlStatus.bits.SwitchingRegulatorPowerDown = 0;
+    device->GphyControlStatus.bits.SwitchingRegulatorPowerDown = 0;
 
     // Set or clear
     // REG_TOP_LEVEL_MISCELLANEOUS_CONTROL_1__NCSI_CLOCK_OUTPUT_DISABLE as
     // desired.
     // Value from Talos: 0x00000080 (unknown, reserved bit set)
-    DEVICE.TopLevelMiscellaneousControl1.bits.NCSIClockOutputDisable = 0;
+    device->TopLevelMiscellaneousControl1.bits.NCSIClockOutputDisable = 0;
 
     reportStatus(STATUS_INIT_HW, 0xf0);
 
@@ -459,16 +458,16 @@ void init_hw(volatile DEVICE_t *device, NVRAMContents_t *nvram)
     bmm.bits.Enable = 1;
     bmm.bits.AttentionEnable = 1;
     bmm.bits.ResetRXMBUFPointer = 1;
-    DEVICE.BufferManagerMode = bmm;
+    device->BufferManagerMode = bmm;
 
     // Set REG_MISCELLANEOUS_LOCAL_CONTROL to AUTO_SEEPROM_ACCESS|GPIO_2_OUTPUT_ENABLE.
-    DEVICE.MiscellaneousLocalControl.bits.AutoSEEPROMAccess = 1;
-    DEVICE.MiscellaneousLocalControl.bits.GPIO2OutputEnable = 1;
+    device->MiscellaneousLocalControl.bits.AutoSEEPROMAccess = 1;
+    device->MiscellaneousLocalControl.bits.GPIO2OutputEnable = 1;
 
     // Setup link-aware power mode.
     // The following must be performed while holding REG_MUTEX_{REQUEST,GRANT}; use bit 4.
-    DEVICE.MutexRequest.r32 |= (1 << 4);
-    while (0 == (DEVICE.MutexGrant.r32 & (1 << 4)))
+    device->MutexRequest.r32 |= (1 << 4);
+    while (0 == (device->MutexGrant.r32 & (1 << 4)))
     {
         // Wait for grant.
     }
@@ -477,20 +476,19 @@ void init_hw(volatile DEVICE_t *device, NVRAMContents_t *nvram)
     RegDEVICELinkAwarePowerModeClockPolicy_t lapmcp;
     lapmcp.r32 = 0;
     lapmcp.bits.MACClockSwitch = DEVICE_LINK_AWARE_POWER_MODE_CLOCK_POLICY_MAC_CLOCK_SWITCH_6_25MHZ;
-    DEVICE.LinkAwarePowerModeClockPolicy = lapmcp;
-    // Set REG_CPMU_CONTROL to zero or more of LINK_AWARE_POWER_MODE_ENABLE, LINK_IDLE_POWER_MODE_ENABLE, LINK_SPEED_POWER_MODE_ENABLE as desired (see NVM
-    // CfgFeature).
+    device->LinkAwarePowerModeClockPolicy = lapmcp;
+    // Set REG_CPMU_CONTROL to zero or more of LINK_AWARE_POWER_MODE_ENABLE, LINK_IDLE_POWER_MODE_ENABLE, LINK_SPEED_POWER_MODE_ENABLE as desired (see NVM CfgFeature).
     RegDEVICECpmuControl_t cpmu_control;
     cpmu_control.r32 = 0;
     cpmu_control.bits.LinkIdlePowerModeEnable = GEN.GenCfgFeature.bits.LinkIdle;
     cpmu_control.bits.LinkAwarePowerModeEnable = GEN.GenCfgFeature.bits.LinkAwarePowerMode;
     cpmu_control.bits.LinkSpeedPowerModeEnable = GEN.GenCfgFeature.bits.LinkSpeedPowerMode;
-    DEVICE.CpmuControl = cpmu_control;
+    device->CpmuControl = cpmu_control;
     // Release grant.
-    DEVICE.MutexGrant.r32 = (1 << 4);
+    device->MutexGrant.r32 = (1 << 4);
 
     // Mask REG_CLOCK_SPEED_OVERRIDE_POLICY__MAC_CLOCK_SPEED_OVERRIDE_ENABLE.
-    DEVICE.ClockSpeedOverridePolicy.bits.MACClockSpeedOverrideEnabled = 0;
+    device->ClockSpeedOverridePolicy.bits.MACClockSpeedOverrideEnabled = 0;
 
     reportStatus(STATUS_INIT_HW, 0xff);
 }
