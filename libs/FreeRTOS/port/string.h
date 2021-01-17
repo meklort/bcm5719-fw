91258ddfc56554553d3d209f8e1b8b58fb3767df
Evan Lojewski <github@meklort.com>
2021-01-17 12:33:03 -0700

    ape: Add initial FreeRTOS support.

---
+++ libs/FreeRTOS/port/string.h
@@ -0,0 +1,10 @@
+
+#ifndef PORT_STRING_H
+#define PORT_STRING_H
+
+#include <types.h>
+
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *s, int c, size_t n);
+
+#endif /* PORT_STRING_H */
