6e4fb077f073ba207c48b23ba2277a4c687fe9de
Evan Lojewski <github@meklort.com>
2022-07-25 19:46:25 -0600

    simulator: Ensure source code is properly formatted.

--- simulator/pci_config.h
+++ simulator/pci_config.h
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 /// Copyright (c) 2018, Evan Lojewski
 /// All rights reserved.
-/// 
+///
 /// Redistribution and use in source and binary forms, with or without
 /// modification, are permitted provided that the following conditions are met:
 ///     * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
 ///     * Neither the name of the <organization> nor the
 ///       names of its contributors may be used to endorse or promote products
 ///       derived from this software without specific prior written permission.
-/// 
+///
 /// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 /// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 /// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -29,7 +29,8 @@
 
 #include <stdint.h>
 
-typedef struct  {
+typedef struct
+{
     uint16_t vendor_id;
     uint16_t device_id;
 
